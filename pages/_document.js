cf16e790e089c399ea9e169387cb3ecd7ae2642c
Cody Cooper <cody_codster@live.com>
2023-05-04 00:51:33 -0500

    Updated font

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Ysabeau&display=swap" rel="stylesheet" />
       </Head>
       <body className='h-screen'>
         <Main />
