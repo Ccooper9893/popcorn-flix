3189061df82636f3fbdada5fa028772e80ff71da
Cody Cooper <cody_codster@live.com>
2023-05-04 00:45:39 -0500

    Updated font

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Thasadith&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
       </Head>
       <body className='h-screen'>
         <Main />
