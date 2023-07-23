087f8a9c79620c9579b7e5ff3989aa2b1557027f
Cody Cooper <cody_codster@live.com>
2023-07-22 20:24:29 -0500

    Updated movie detail page layout

--- components/Details/MovieOverview.js
+++ components/Details/MovieOverview.js
@@ -6,14 +6,16 @@ import Providers from "./Providers";
 import { useEffect, useState } from "react";
 const MovieOverview = ({ movie, videoKey, similarMovies, button, handlePageChange }) => {
 
-    const [overViewTab, setOverviewTab] = useState(true);
+    const [videoTab, setVideoTab] = useState(true);
+    const [overViewTab, setOverviewTab] = useState(false);
     const [providersTab, setProvidersTab] = useState(false);
     const [similarTab, setSimilarTab] = useState(false);
 
     useEffect(() => {
+        setVideoTab(true);
         setSimilarTab(false);
         setProvidersTab(false);
-        setOverviewTab(true);
+        setOverviewTab(false);
     }, [movie])
 
     //Format revenue $$$
@@ -37,11 +39,52 @@ const MovieOverview = ({ movie, videoKey, similarMovies, button, handlePageChang
         width: '850px',
     };
     const videoOptions = formatVideo(mobileDimensions, desktopDimensions, 1);
-
     return (
-        <div className="flex flex-col my-6 mx-4">
-            {!videoKey && <h1 className="text-center">No videos found</h1>}
-            {videoKey && <YouTube className="mx-auto mt-2 shadow-lg shado shadow-black z-30" videoId={videoKey} opts={videoOptions} />}
+        <div className="flex flex-col mt-4">
+            <h1 className="text-2xl text-center text-white font-bold md:text-2xl">{movie.original_title}</h1>
+            <div className="tabs align-top justify-center">
+                <a className={overViewTab ? 'tab tab-bordered tab-active text-yellow-600 text-xl' : 'tab tab-bordered text-xl'} onClick={() => { setOverviewTab(true); setProvidersTab(false); setSimilarTab(false); }}>Overview</a>
+                <a className={videoTab ? 'tab tab-bordered tab-active text-yellow-600 text-xl' : 'tab tab-bordered text-xl'} onClick={() => { videoTab(true); setProvidersTab(false); setSimilarTab(false); }}>Overview</a>
+                <a className={overViewTab ? 'tab tab-bordered tab-active text-yellow-600 text-xl' : 'tab tab-bordered text-xl'} onClick={() => { setOverviewTab(true); setProvidersTab(false); setSimilarTab(false); }}>Overview</a>
+                <a className={providersTab ? 'tab tab-bordered tab-active text-yellow-600 text-xl' : 'tab tab-bordered text-xl'} onClick={() => { setOverviewTab(false); setProvidersTab(true); setSimilarTab(false); }}>Providers</a>
+                <a className={similarTab ? 'tab tab-bordered tab-active text-yellow-600 text-xl' : 'tab tab-bordered text-xl'} onClick={() => { setOverviewTab(false); setProvidersTab(false); setSimilarTab(true); }}>Similar</a>
+            </div>
+            <div className="text-center">
+
+                {overViewTab && (
+                    <div className="mt-3">
+                        <p className="text-lg lg:w-3/5 mx-auto">{movie.overview}</p>
+                        <Rating vote={movie.vote_average} />
+                        <h2 className="">Votes:  {movie.vote_count}</h2>
+                        <h2 className="">Released: {movie.release_date}</h2>
+                        <h2 className="">Revenue: {revenue}<small></small></h2>
+                        <h2 className="">Budget: {budget}<small></small></h2>
+                    </div>
+                )}
+                {videoTabTab && (
+                    <div className="w-full">
+                        {!videoKey && <h1 className="text-center">No videos found</h1>}
+                        {videoKey && <YouTube className="mx-auto mt-2 shadow-lg shadow-black z-30" videoId={videoKey} opts={videoOptions} />}
+                    </div>
+                )}
+                {providersTab && (
+                    <div className="w-full">
+                        <Providers provider={movie["watch/providers"].results.US} />
+                    </div>
+                )}
+                {similarTab && (
+                    <SimilarMovies similarMovies={similarMovies} button={button} handlePageChange={handlePageChange} />
+                )}
+            </div>
+        </div>
+
+    )
+};
+
+export default MovieOverview;
+
+/* 
+
             <div className="flex flex-col mt-4">
                 <h1 className="text-2xl text-center text-white font-bold md:text-2xl">{movie.original_title}</h1>
                 <div className="tabs align-top justify-center">
@@ -71,9 +114,5 @@ const MovieOverview = ({ movie, videoKey, similarMovies, button, handlePageChang
                     )}
                 </div>
             </div>
-        </div>
-    )
-};
 
-export default MovieOverview;
-
+*/
