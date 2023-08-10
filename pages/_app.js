3ab2e5449bb5edcc1da7d0b90643097561fb4376
Cody Cooper <cody_codster@live.com>
2023-08-10 14:32:01 -0500

    Added Google Analytics package

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import Head from 'next/head';
 import Navbar from '@/components/Navbar/Navbar';
 import { SearchProvider } from '@/utils/searchContext';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Bring Popcorn</title>
       </Head>
+      <GoogleAnalytics trackPageViews />
       <SearchProvider>
         <Navbar>
           <div className='h-screen pt-16 lg:pt-20'>
