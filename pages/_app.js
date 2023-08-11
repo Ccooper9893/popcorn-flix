99bf79272708622f7ebd864d339c65ae6296321c
Cody Cooper <cody_codster@live.com>
2023-08-11 15:17:59 -0500

    Setting up Google Tag Manager for practice

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,15 @@ import Navbar from '@/components/Navbar/Navbar';
 import { SearchProvider } from '@/utils/searchContext';
 import Script from 'next/script';
 
+const tagManagerArgs = {
+  id: `GTM-${process.env.NEXT_PUBLIC_GTM_ID}`,
+};
+
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
   return (
     <div>
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}/>
