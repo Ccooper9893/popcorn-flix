61707ca152bd2d27fe15ab7c0704ed1fc72e7b59
Cody Cooper <cody_codster@live.com>
2023-08-10 20:39:12 -0500

    Testing

--- pages/_app.js
+++ pages/_app.js
@@ -7,14 +7,14 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}/>
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
       </Script>
       <Head>
