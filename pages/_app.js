cb75b0e0deedb266c5950fc225c5d87053acf021
Cody Cooper <cody_codster@live.com>
2023-08-11 17:42:18 -0500

    Debugging GTM

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,9 @@ import Navbar from '@/components/Navbar/Navbar';
 import { SearchProvider } from '@/utils/searchContext';
 import Script from 'next/script';
 
-const tagManagerArgs = {
-  id: `GTM-${process.env.NEXT_PUBLIC_GTM_ID}`,
-};
-
 export default function App({ Component, pageProps }) {
-
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs)
-  }, [])
-  return (
+    return (
     <div>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}/>
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
