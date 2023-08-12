f65b2f50ee6739f1ff5c0c6a23e9a5607be0eb34
Cody Cooper <cody_codster@live.com>
2023-08-12 13:09:28 -0500

    Debugging GTM

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,6 @@ export default function App({ Component, pageProps }) {
 
     return ( 
     <div>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -19,13 +18,15 @@ export default function App({ Component, pageProps }) {
           gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
       </Script>
-      <Script strategy="afterInteractive" id="gtm-script">
-        
+      <Script id="google-tag-manager" strategy="afterInteractive">
         {`
-          // Empty script tag because GTM is loaded through react-gtm-module
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
         `}
-        
-      </Script>
+    </Script>
       <Head>
         <title>Bring Popcorn</title>
       </Head>
