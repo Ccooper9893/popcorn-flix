d3d449c9a68c462446626d29da9675f3b6407e8a
Cody Cooper <cody_codster@live.com>
2023-08-12 12:41:43 -0500

    Debugging Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,13 @@ import Head from 'next/head';
 import Navbar from '@/components/Navbar/Navbar';
 import { SearchProvider } from '@/utils/searchContext';
 import Script from 'next/script';
+import TagManager from 'react-gtm-module';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Initialize GTM
+    TagManager.initialize({ gtmId: process.env.GTM });
+  }, []);
     return ( 
     <div>
       <Script id="google-analytics">
@@ -14,6 +19,12 @@ export default function App({ Component, pageProps }) {
           gtag('js', new Date());
  
           gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Script strategy="afterInteractive" id="gtm-script">
+        
+        {`
+          // Empty script tag because GTM is loaded through react-gtm-module
         `}
       </Script>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -8,34 +7,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Ysabeau&display=swap" rel="stylesheet" />
-        <Script
-              id="gmtscript"
-              type="text/javascript"
-              async
-              dangerouslySetInnerHTML={{
-                __html: `
-              (function(w, d, s, l, i) {
-                    w[l] = w[l] || []
-                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-                    const f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != "dataLayer" ? "&l=" + l : ""
-                    j.async = true
-                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
-                    f.parentNode.insertBefore(j, f)
-                  })(window,document,'script','dataLayer',"${process.env.GTM}")`,
-              }}
-            />
       </Head>
       <body className='h-screen'>
-      <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         <Main />
         <NextScript />
       </body>
